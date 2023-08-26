dc5a55fd3463da69454deb2a7d171d1b99ef8081
Marcus Dillavou <line72@line72.net>
2023-08-26 10:11:58 -0500

    Build Android 1.2.0-1

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ CONFIG = transmogrifier.Config(
         app_store_url = 'https://apps.apple.com/us/app/go-tuscaloosa/id1495548625'
     ),
     android_config = transmogrifier.MontclairAndroidConfig(
-        version = '1.0.2',
+        version = '1.2.0',
         revision = 1,
         app_id = 'com.gotransitapp.tuscaloosa',
         play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.tuscaloosa'
